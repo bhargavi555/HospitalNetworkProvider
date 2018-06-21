37872d8b4e24a9d7dc4b7200cd9f2e3ac855d75f
Bhargavi <bhargavi.pegasian@gmail.com>
2018-06-21 10:34:14 -0500

    controller commit

--- demo/src/main/java/com/networkprovider/controller/NetworkProviderController.java
+++ demo/src/main/java/com/networkprovider/controller/NetworkProviderController.java
@@ -1,11 +1,18 @@
 package com.networkprovider.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class NetworkProviderController {
+	
+
+
 	
 	@RequestMapping(value="/hospitals",method=RequestMethod.GET)
 	public String hospitals() {

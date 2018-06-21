029cba23c52a147ea0343edebdb1548d63df3f9d
ujjvalthakkar <hemantthakkar.u@husky.neu.edu>
2018-06-21 11:29:15 -0400

    Adding DAO,POJOs

--- demo/src/main/java/com/Altimetrik/Controller/NetworkProviderController.java
+++ demo/src/main/java/com/Altimetrik/Controller/NetworkProviderController.java
@@ -1,4 +1,4 @@
-package com.networkprovider.controller;
+package com.Altimetrik.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
